7bef330f517f83c6d0f566aca34ea810fb730947
mhmmdsyahputra <putra.cokers@gmail.com>
2023-01-09 15:36:56 +0700

    middleware complate, membuat rute tertentu yg hanya bisa diases jika sudah login saja atau sudah bisa melewati middleware

--- routes/signin_signup.js
+++ routes/signin_signup.js
@@ -7,76 +7,96 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'eriyfbercbieobu3hrurebuberHBububUOBUOUBuo3728u'
 
-//CREATE
+//REGISTRASI NEW USER--------------------------------------------------------------------
 router.post("/registrasi", async (req, res) => {
-    const {firstname,lastname,email,password} = req.body
-
-    const encrypPassword = await bcryptjs.hash(password,10)
-
+    const { firstname, lastname, email, password } = req.body
+    
+    const encrypPassword = await bcryptjs.hash(password, 10)
+    
     const newUser = new Registrasi({
         firstname: firstname,
         lastname: lastname,
         email: email,
         password: encrypPassword,
     })
-    
+
     try {
-        const olduser = await Registrasi.findOne({email})
+        const olduser = await Registrasi.findOne({ email })
         if (olduser) {
             return res.send({ error: "Email Sudah Dipakai" });
-         }
+        }
 
         const reg = await newUser.save()
         res.send({ message: "Account Created Successfully" });
 
     } catch (error) {
-        res.json({message: error})
+        res.json({ message: error })
     }
 
 });
 
-router.post('/login', async (req,res)=>{
-    const {email,password} = req.body
+//LOGIN USER--------------------------------------------------------------------
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body
 
-    const user = await Registrasi.findOne({email})
+    const user = await Registrasi.findOne({ email })
 
-    if(!user){
-        return res.json({status: 'error', message : 'Email Tidak Terdaftar!'})
+    if (!user) {
+        return res.json({ status: 'error', message: 'Email Tidak Terdaftar!' })
     }
 
-    if(await bcryptjs.compare(password, user.password)){
-        const token = jwt.sign({email: user.email,}, JWT_SECRET)
+    if (await bcryptjs.compare(password, user.password)) {
+        const token = jwt.sign({ email: user.email, }, JWT_SECRET)
 
         if (res.status(201)) {
-            return res.json({status: 'ok', data: token, user: user})
-        }else{
-            return  res.json({error: 'Error '})
+            return res.json({ status: 'ok', data: token, user: user })
+        } else {
+            return res.json({ error: 'Error ' })
         }
     }
 
-    res.json({status: 'error', message: 'Password Salah!'})
+    res.json({ status: 'error', message: 'Password Salah!' })
 })
 
-router.post('/onlyAdmin', async (req,res)=>{
+// MIDDLEWARE-------------------------------------------------------------------------------
+router.use((req, res, next) => {
+    // get token from header
+    const token = JSON.parse(req.headers['x-access-token']);
+    // console.log(token.token)
+
+    // verify token
+    jwt.verify(token.token, JWT_SECRET, (error) => {
+        if (error) {
+            return res.status(401).json({ status: 'error', message: 'Token tidak valid' });
+        } else {
+            next();
+        }
+    });
+});
+
+// ROUTE YANG HANYA BISA AKSES SAMA ADMIN SAJA ATAU YG SUDAH LOGIN------------------------------
+
+router.post('/onlyAdmin', async (req, res) => {
     const { token } = req.body
+    // console.log(token);
 
-    console.log(token);
+    try {
+        const user = jwt.verify(token.token, JWT_SECRET)
 
-    try{
-        const user = jwt.verify(token.token,JWT_SECRET)
-        
         const userEmail = user.email
 
-        Registrasi.findOne({email: userEmail})
-        .then((data)=>{
-            res.send({ status:'ok', data:data })
-        }).catch((err)=>{
-            res.send({ status:'err', data:err })
-        })
-    }catch(err){
+        Registrasi.findOne({ email: userEmail })
+            .then((data) => {
+                res.send({ status: 'ok', data: data })
+            }).catch((err) => {
+                res.send({ status: 'err', data: err })
+            })
+    } catch (err) {
         // console.log(err);
-        res.json({status: 'error', message: 'Token is invalid'});
+        res.json({ status: 'error', message: 'Token is invalid' });
     }
+
+    // res.json({ message: 'Successfully authenticated' });
 })
 
 
